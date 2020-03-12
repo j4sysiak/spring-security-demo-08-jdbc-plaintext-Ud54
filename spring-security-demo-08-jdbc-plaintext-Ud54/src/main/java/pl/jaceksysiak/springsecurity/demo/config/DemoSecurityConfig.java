f8073dcb767b5b702c6e67c2cf389f55940d3983
Jacek <Jacek@BERLIN>
2020-03-12 06:21:39 +0100

    kk

--- spring-security-demo-08-jdbc-plaintext-Ud54/src/main/java/pl/jaceksysiak/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-08-jdbc-plaintext-Ud54/src/main/java/pl/jaceksysiak/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,28 +1,34 @@
 package pl.jaceksysiak.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	// add a reference to our security data source
+	@Autowired
+	private DataSource securityDataSource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		// add our users for in memory authentication
+//		// add our users for in memory authentication		
+//		UserBuilder users = User.withDefaultPasswordEncoder();
+//		
+//		auth.inMemoryAuthentication()
+//			.withUser(users.username("aa").password("aa").roles("EMPLOYEE"))
+//			.withUser(users.username("bb").password("bb").roles("EMPLOYEE", "MANAGER"))
+//			.withUser(users.username("cc").password("cc").roles("EMPLOYEE", "ADMIN"));
 		
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		
-		auth.inMemoryAuthentication()
-			.withUser(users.username("aa").password("aa").roles("EMPLOYEE"))
-			.withUser(users.username("bb").password("bb").roles("EMPLOYEE", "MANAGER"))
-			.withUser(users.username("cc").password("cc").roles("EMPLOYEE", "ADMIN"));
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 	}
 
 	@Override
